1aef73da5f30e2e4a57b149ba45eae0471149403
Benjamin Aho <Benjamin.aho27@gmail.com>
2023-02-13 23:50:59 -0500

    updated files added

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var app = document.querySelector('.typewriter');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+})
+
+typewriter.typeString('Web Developer')
+    .pauseFor(2000)
+    .deleteAll()
+    .typeString('Designer')
+    .pauseFor(2000)
+    .deleteAll()
+    .typeString('Creative')
+    .pauseFor(2000)
+    .deleteAll()
+    .start();
+// Tab toggle
+
+
+// isotope js
+
+
+// Testimonial Slider
+
+
+// Scroll to top
+
+
+//Navbar
+
+
